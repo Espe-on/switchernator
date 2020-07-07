b9806866d6e8d99077f5903d3ee94db367db77c6
espe-on <contact@josephcrawley.dev>
2020-07-07 09:53:14 +0100

    added check message for input values

--- switchernator.tcl
+++ switchernator.tcl
@@ -6,5 +6,14 @@ set username [lindex $argv 0]
 set password [lindex $argv 1]
 set listname [lindex $argv 2]
 
-puts "username is $username, password is $password, listname is $listname"
+puts "Please check the following values are correct."
+puts "Username is $username, Password is $password, Listname is $listname"
+puts "Are these values correct? (y/n)" 
+gets stdin areValuesCorrect
+if {$areValuesCorrect != y} {
+    puts "Please re-run script with correct values"
+    exit 1 
+} else {
+    puts "values valid"
+}
 
